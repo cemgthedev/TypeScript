31a42b478079455721b7e947c6ed7df4ed81c398
cemgthedev <cemg.the.dev@gmail.com>
2023-02-12 11:10:00 -0300

    estudando tipagem de estados

---
+++ 03_outras_tipagens/tipagem_de_estados.ts
@@ -0,0 +1,26 @@
+// import { useState } from 'react';
+
+type CardProps = {
+    time: string,
+    name: string
+}
+
+/*
+export function List({ list }: CardProps[]) {
+    const [count, setCount] = useState<number>(0);
+
+    return (
+        <div>
+            <h1>Atividades</h1>
+            list.map((item) => (
+                <div key={ item.time }>
+                    <label>Name: { item.name }</label>
+                    <p>Time: { item.time }</p>
+                    <p>Count: { count }</p>
+                    <Button handleCount={setCount} />
+                </div>
+            ));
+        </div>
+    );
+}
+*/
